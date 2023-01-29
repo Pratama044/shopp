51feb1811e87e029eed6b3f1a1773743c3c3b9c2
Kaizen <121414529+Pratama044@users.noreply.github.com>
2023-01-29 20:27:56 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// const navBar = document.querySelector(".search-container");
+
+// window.addEventListener("scroll", () =>{
+//     const windowPosition = window.scrollY > 0;
+//     navBar.classList.toggle("crolling-active", windowPosition);
+// });
